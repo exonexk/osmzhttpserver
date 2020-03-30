265941f765c9c19fff1d73fe2446fcc8a8506585
Lukáš Klobasa <lukas.klobasa@vsb.cz>
2020-03-30 19:48:43 +0200

    Přidání projektu HTTP server

--- app/src/main/java/com/vsb/kru13/osmzhttpserver/CamActivity.java
+++ app/src/main/java/com/vsb/kru13/osmzhttpserver/CamActivity.java
@@ -58,7 +58,7 @@ public class CamActivity extends Activity {
         startStream.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               
+
                 timerTask.run();
             }
         });
